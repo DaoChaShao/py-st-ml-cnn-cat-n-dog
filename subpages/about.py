dfd0bbc4c575cab063a2f395578e1845ecdfcc60
DaoChaShao <27882272@qq.com>
2025-09-25 10:26:50 +0800

    feat: add about.py for application information display

---
+++ subpages/about.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     :   2025/9/24 22:51
+# @Author   :   Shawn
+# @Version  :   Version 0.1.0
+# @File     :   about.py
+# @Desc     :   
+
+from streamlit import title, expander, caption
+
+title("**Application Information**")
+with expander("About this application", expanded=True):
+    caption("-")
